7c4a1573c06e9d6812b7649c244a709cc2f32a1b
elliot.woods@gmail.com <elliot.woods@gmail.com@52398a2f-5f8c-4237-c580-2f6807e5d9c9>
2010-10-22 08:13:02 +0000

    adding source for CV GUI

---
+++ oF/addons/ofxCVgui/src/GUIConstants.h
@@ -0,0 +1,25 @@
+#pragma once
+/*
+ *  GUIConstants.h
+ *  pixel correlation
+ *
+ *  Created by Elliot Woods on 04/07/2010.
+ *  Copyright 2010 Kimchi and Chips. All rights reserved.
+ *
+ */
+
+#define GUI_INTERFACE_DISPLAY_TIME 2
+
+#define GUI_FONT_SIZE 10
+#define GUI_STATUS_LINES 2
+
+enum enumGUIButtonState { button_wait, button_over, button_down };
+enum enumGUIButtonType {button_bang, button_toggle};
+
+enum enumShowCursor { cursor_none, cursor_x, cursor_y, cursor_xy };
+enum enumScreenType { enum_texture, enum_histogram };
+
+#define GUI_INTERFACE_BUTTON_SIZE 16
+#define GUI_INTERFACE_BORDER_LINE_WIDTH 4
+
+#define GUI_HISTOGRAM_TEXTURE_RESOLUTION 512
