47606b6aa8e4adafa8588771b113e5d2fc2fb919
elliot.woods@gmail.com <elliot.woods@gmail.com@52398a2f-5f8c-4237-c580-2f6807e5d9c9>
2010-10-27 05:48:36 +0000

    remove old PC Encode version, replace with 3.0 directory

--- trunk/oF/apps VS/PC Encode VS/PCManager.cpp
+++
@@ -1,9 +0,0 @@
-/*
- *  PCManager.cpp
- *  PCDecode
- *
- *  Created by Elliot Woods on 25/10/2009.
- *  Copyright 2009 Kimchi and Chips. All rights reserved.
- *
- */
-
--- trunk/oF/apps VS/PC Encode VS/src/lookup.frag
+++
@@ -1,6 +0,0 @@
-varying vec3 normal;
-
-void main( void )
-{  
-   gl_FragColor= vec4(normal, 1.0);
-}
