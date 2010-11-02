d34938d6945f8eccaa76ff8d5c4cbb26820e25ea
elliot.woods@gmail.com <elliot.woods@gmail.com@52398a2f-5f8c-4237-c580-2f6807e5d9c9>
2010-11-02 13:08:39 +0000

    added widgets screen and slider widget

---
+++ trunk/oF/addons/ofxCVgui/src/scrWidgets.cpp
@@ -0,0 +1,75 @@
+/*
+ *  scrWidgets.cpp
+ *  PC Encode
+ *
+ *  Created by Elliot Woods on 02/11/2010.
+ *  Copyright 2010 Kimchi and Chips. All rights reserved.
+ *
+ */
+
+#include "scrWidgets.h"
+
+#define FOREACH_WIDGET for (int iWidget=0; iWidget<_widgets.size(); iWidget++)
+
+scrWidgets::scrWidgets(string _caption)
+: scrBase(cursor_none, true, _caption)
+{
+	_sumHeights=WIDGET_PADDING;
+}
+
+void scrWidgets::mouseDown(int x, int y)
+{
+	int currentx, currenty, currentw, currenth;
+	getLiveBounds(currentx, currenty, currentw, currenth);
+	
+	if (y<0)
+		return;
+	
+	int yOffset=y-currenty;
+	
+	FOREACH_WIDGET
+	{
+		if  (yOffset <= _widgets[iWidget]->height)
+		{
+			_widgets[iWidget]->mouseDown(x,y);
+			break;
+		}
+		
+		yOffset -= _widgets[iWidget]->height;
+	}
+		
+}
+
+void scrWidgets::push(wdgBase *widget)
+{
+	int x, y, w, h;
+	getLiveBounds(x, y, w, h);
+	
+	widget->setBounds(x+WIDGET_PADDING, y+_sumHeights+WIDGET_PADDING,
+					  w - 2*WIDGET_PADDING);
+	
+	_widgets.push_back(widget);
+	
+	_sumHeights += widget->height + 2*WIDGET_PADDING;
+}
+
+void scrWidgets::doResize()
+{
+	int x, y, w, h;
+	getLiveBounds(x, y, w, h);
+	
+	_sumHeights = WIDGET_PADDING;
+	
+	FOREACH_WIDGET
+	{
+		_widgets[iWidget]->setBounds(x+WIDGET_PADDING, y+_sumHeights+WIDGET_PADDING,
+										   w - 2*WIDGET_PADDING);
+		_sumHeights += _widgets[iWidget]->height + 2*WIDGET_PADDING;
+	}
+}
+
+void scrWidgets::drawContent()
+{
+	FOREACH_WIDGET
+		_widgets[iWidget]->draw();
+}
---
+++ trunk/oF/addons/ofxCVgui/src/wdgBase.cpp
@@ -0,0 +1,31 @@
+/*
+ *  wdgBase.cpp
+ *  PC Encode
+ *
+ *  Created by Elliot Woods on 02/11/2010.
+ *  Copyright 2010 Kimchi and Chips. All rights reserved.
+ *
+ */
+
+#include "wdgBase.h"
+
+wdgBase::wdgBase(string _caption) :
+height(_height)
+{
+	caption = _caption;
+	_height = 50;
+}
+
+void wdgBase::setBounds(int x, int y, int width)
+{
+	_x = x;
+	_y = y;
+	
+	_width = width;
+}
+
+bool wdgBase::isHit(int x, int y)
+{
+	return (x>=_x && y>=_y &&
+			x<=_x+_width && y<=_y+_height);
+}
