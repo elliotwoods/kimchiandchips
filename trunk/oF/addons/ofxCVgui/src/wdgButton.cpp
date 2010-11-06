b051c6605cff63e1405491114530c66aa244814a
elliot.woods@gmail.com <elliot.woods@gmail.com@52398a2f-5f8c-4237-c580-2f6807e5d9c9>
2010-11-06 07:28:48 +0000

    Added 'enabled' for button Also bang will draw now before banging

--- trunk/oF/addons/ofxCVgui/src/wdgButton.cpp
+++ trunk/oF/addons/ofxCVgui/src/wdgButton.cpp
@@ -18,6 +18,9 @@ wdgBase(caption)
 	_valueToggle = &myValue;
 	
 	_wasBang = 0;
+	
+	enabled = true;
+	_hasDrawn = true;
 }
 
 wdgButton::wdgButton(string caption) :
@@ -28,6 +31,9 @@ wdgBase(caption)
 	_valueBang = false;
 
 	_wasBang = 0;
+	
+	enabled = true;
+	_hasDrawn = true;
 }
 
 void wdgButton::draw()
@@ -40,6 +46,7 @@ void wdgButton::draw()
 	ofPushStyle();
 	ofSetLineWidth(1);
 	ofFill();
+	
 	if (currentValue)
 		ofSetColor(255, 255, 255);
 	else
@@ -52,7 +59,12 @@ void wdgButton::draw()
 		ofPushStyle();
 		ofSetLineWidth(1);
 		ofNoFill();
-		ofSetColor(255, 255, 255);
+		
+		if (enabled)
+			ofSetColor(255, 255, 255);
+		else
+			ofSetColor(100, 100, 100);
+		
 		ofRect(_x, _y, _width, _height);
 		ofPopStyle();
 	}
@@ -62,27 +74,33 @@ void wdgButton::draw()
 	else
 		ofSetColor(255, 255, 255);
 	
+	if (!enabled)
+		ofSetColor(100, 100, 100);
+	
 	ofDrawBitmapString(caption, _x+_width/2-4*caption.length(),
 					   _y+_height/2+5);
 	ofPopStyle();
 	
-	
-	
 	if (_wasBang>0)
 		_wasBang--;
+	
+	_hasDrawn = true;
 
 }
 void wdgButton::mouseDown(int x, int y)
 {
-	if (_isBang)
-		_valueBang = true;
-	else
-		*_valueToggle = !*_valueToggle;
+	if (enabled)
+		if (_isBang)
+			_valueBang = true;
+		else
+			*_valueToggle = !*_valueToggle;
+	
+	_hasDrawn=false;
 }
 
 bool wdgButton::getBang()
 {
-	if (_isBang)
+	if (_isBang && _hasDrawn)
 	{
 		if (_valueBang)
 		{
