2a6ec79cf73ec0244f8b1e6c40c1f80a0e3ff17c
elliot.woods@gmail.com <elliot.woods@gmail.com@52398a2f-5f8c-4237-c580-2f6807e5d9c9>
2011-04-11 16:29:52 +0000

    before per-interleave threshold

--- oF/apps VS/PC Encode 3.0/scrSelectBounds.cpp
+++ oF/apps VS/PC Encode 3.0/scrSelectBounds.cpp
@@ -10,8 +10,8 @@
 
 scrSelectBounds::scrSelectBounds(string caption) :
 scrPointCloud(caption),
-lbf(-0.8,-0.6,0.9),
-rtb(0.4,0.5,3.0)
+lbf(-0.5,-0.6,0.9),
+rtb(0.5,0.3,1.4)
 {
     
     shader.setup("selectBounds");
